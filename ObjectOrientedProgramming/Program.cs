7a74e0222dca6b3a0b92ad0f89cd9b446717d7ff
Milkriver <mapav4@yandex.ru>
2022-02-10 19:57:17 +0500

    ObjectOrientedProgramming добавлены геттер и сеттер

--- ObjectOrientedProgramming/Program.cs
+++ ObjectOrientedProgramming/Program.cs
@@ -15,14 +15,41 @@ namespace ObjectOrientedProgramming
 
     class Employee : Person
     {
-        public string Inn { get; private set; }
+        protected string _inn;
+
+        public string InnWrapper {
+            get => _inn;
+            //string getInn () {
+            // return _inn
+            //}
+
+            //private void setInn (string value) {
+            // _inn = value
+            //}
+            private set
+            {
+                if (value.Length != 12)
+                    throw new Exception();
+
+                _inn = value;
+            }
+        }
+
+        public string InnAutoWrapper { get; set; }
 
         public Employee(string name, int age, string inn)
             : base(name, age) 
         {
-            Inn = inn;
+            //setInn(inn)
+            InnWrapper = inn;
+            InnAutoWrapper = inn;
 
-        }  
+        } 
+        
+        void SetInnExclusiveMethod (string governmentService, string newInn)
+        {
+            InnWrapper = newInn;
+        }
     }
 
 
@@ -32,7 +59,8 @@ namespace ObjectOrientedProgramming
         {
             Console.WriteLine("Hello World!");
             var e = new Employee("Anton", 32, "123inn");
-            Console.WriteLine(e.Inn);
+            e.InnAutoWrapper = "123";
+            Console.WriteLine(e.InnWrapper);
         }
     }
 }
