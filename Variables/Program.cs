db5b5ae78458c0fd4f93fa1d61a1ba32c8295ade
Milkriver <mapav4@yandex.ru>
2022-02-13 20:33:38 +0500

    Обучение - переменные

--- Variables/Program.cs
+++ Variables/Program.cs
@@ -5,32 +5,37 @@ namespace Variables
     class Product1
     {
         public string name;
+        public string description;
         public void GetInfo()
         {
             Console.WriteLine($"Название товара: {name}");
+            Console.WriteLine($"Описание товара: {description}");
             Console.WriteLine();
         }
     }
     class Product2
     {
         public string name;
-        public Product2(string name)
+        public string description;
+        public Product2(string name, string description)
         {
             this.name = name;
+            this.description = description;
         }
         public void GetInfo()
         {
             Console.WriteLine($"Название товара: {name}");
+            Console.WriteLine($"Описание товара: {description}");
             Console.WriteLine();
         }
     }
 
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
             var pen = new Product1();
-            var pencil = new Product2("Карандаш");
+            var pencil = new Product2("Карандаш", "Описание карандаша");
             pen.name = "Ручка";
             pen.GetInfo();
             pencil.GetInfo();
